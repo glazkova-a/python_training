b06d616d456c6538b80458a1ec5e342a06aafa71
anna.g <angie.bts@gmail.com>
2021-11-09 15:35:49 -0500

    Parameter object has been implemented.

--- add_new_contact.py
+++ add_new_contact.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import unittest
+from group1 import Group1
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
@@ -13,11 +14,11 @@ class AddNewContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.create_new_contact(wd, name="A", middlen="B", surname="C", nick="D", company="Company1",
+        self.create_new_contact(wd, Group1(name="A", middlen="B", surname="C", nick="D", company="Company1",
                                 address="111, Street", home_phone="12345678", mobile_phone="12345678",
                                 work_phone="12345678", email="aaaa@gmail.com", email2="aaaa@gmail.com",
                                 email3="aaaa@gmail.com", birth_day="3", birth_month="March", birth_year="1987",
-                                ann_day="16", ann_month="August", ann_year="2000", address2="222, Street")
+                                ann_day="16", ann_month="August", ann_year="2000", address2="222, Street"))
         self.return_to_home_page(wd)
         self.logout(wd)
 
