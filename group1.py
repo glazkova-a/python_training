e6a0f052b4f22b44aebb31c79a86274a073b8b4f
anna.g <angie.bts@gmail.com>
2021-11-09 15:27:28 -0500

    Group1 created.

---
+++ group1.py
@@ -0,0 +1,26 @@
+class Group1:
+    def __init__(self,name, middlen, surname, nick, company, address, home_phone, mobile_phone, work_phone, email, email2, email3, birth_day, birth_month, birth_year, ann_day, ann_month, ann_year, address2):
+        self.name = name
+        self.middlen = middlen
+        self.surname = surname
+        self.nick = nick
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.birth_month = birth_month
+        self.birth_day = birth_day
+        self.birth_year = birth_year
+        self.ann_day = ann_day
+        self.ann_year = ann_year
+        self.ann_month = ann_month
+        self.address2 = address2
+
+
+
+
+
