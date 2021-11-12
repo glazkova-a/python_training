d8c6f65021d53ec65fe508375ecc22fd24ee5b5c
anna.g <angie.bts@gmail.com>
2021-11-12 14:08:11 -0500

    New test suite consisted of 3 test created. Fixture and helpers were added.

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -1,119 +1,29 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-import unittest
+import pytest
 from model.contact_model import Contact
+from fixture.application import Application
 
-class AddNewContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-       
-    
-    def test_add_new_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.create_new_contact(wd, Contact(name="A", middlen="B", surname="C", nick="D", company="Company1",
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_new_contact(app):
+
+    app.open_home_page()
+    app.login(username="admin", password="secret")
+    app.create_new_contact(Contact(name="A", middlen="B", surname="C", nick="D", company="Company1",
                                 address="111, Street", home_phone="12345678", mobile_phone="12345678",
                                 work_phone="12345678", email="aaaa@gmail.com", email2="aaaa@gmail.com",
                                 email3="aaaa@gmail.com", birth_day="3", birth_month="March", birth_year="1987",
                                 ann_day="16", ann_month="August", ann_year="2000", address2="222, Street"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self, wd):
-        # go to home page
-        wd.find_element_by_link_text("home page").click()
-
-    def create_new_contact(self, wd, contact_model):
-        # init new contact creation
-        wd.find_element_by_link_text("add new").click()
-        # names section
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact_model.name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact_model.middlen)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact_model.surname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact_model.nick)
-        # company section
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact_model.company)
-        # address section
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact_model.address)
-        # phones section
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact_model.home_phone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact_model.mobile_phone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact_model.work_phone)
-        # emails section
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact_model.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact_model.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact_model.email3)
-        # birthday section
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact_model.birth_day)
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact_model.birth_month)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact_model.birth_year)
-        # anniversary section
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact_model.ann_day)
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact_model.ann_month)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact_model.ann_year)
-        # secondary section
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact_model.address2)
-        # submit form
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def login(self, wd, username, password):
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        # open home page
-        wd.get("http://localhost/addressbook/index.php")
-
-    def tearDown(self):
-        self.wd.quit()
+    app.return_to_home_page()
+    app.logout()
 
 
-if __name__ == "__main__":
-    unittest.main()
+
+
