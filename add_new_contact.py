0098b6004fb73b5a5f94aae502cb4f193f974edb
anna.g <angie.bts@gmail.com>
2021-11-12 07:56:24 -0500

    Group changed to Contact, group1 changed to contact_model

--- add_new_contact.py
+++ add_new_contact.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import unittest
-from group1 import Group
+from contact_model import Contact
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
@@ -14,7 +14,7 @@ class AddNewContact(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.create_new_contact(wd, Group(name="A", middlen="B", surname="C", nick="D", company="Company1",
+        self.create_new_contact(wd, Contact(name="A", middlen="B", surname="C", nick="D", company="Company1",
                                 address="111, Street", home_phone="12345678", mobile_phone="12345678",
                                 work_phone="12345678", email="aaaa@gmail.com", email2="aaaa@gmail.com",
                                 email3="aaaa@gmail.com", birth_day="3", birth_month="March", birth_year="1987",
@@ -30,74 +30,74 @@ class AddNewContact(unittest.TestCase):
         # go to home page
         wd.find_element_by_link_text("home page").click()
 
-    def create_new_contact(self, wd, group1):
+    def create_new_contact(self, wd, contact_model):
         # init new contact creation
         wd.find_element_by_link_text("add new").click()
         # names section
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(group1.name)
+        wd.find_element_by_name("firstname").send_keys(contact_model.name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(group1.middlen)
+        wd.find_element_by_name("middlename").send_keys(contact_model.middlen)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(group1.surname)
+        wd.find_element_by_name("lastname").send_keys(contact_model.surname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(group1.nick)
+        wd.find_element_by_name("nickname").send_keys(contact_model.nick)
         # company section
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(group1.company)
+        wd.find_element_by_name("company").send_keys(contact_model.company)
         # address section
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(group1.address)
+        wd.find_element_by_name("address").send_keys(contact_model.address)
         # phones section
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(group1.home_phone)
+        wd.find_element_by_name("home").send_keys(contact_model.home_phone)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(group1.mobile_phone)
+        wd.find_element_by_name("mobile").send_keys(contact_model.mobile_phone)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(group1.work_phone)
+        wd.find_element_by_name("work").send_keys(contact_model.work_phone)
         # emails section
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(group1.email)
+        wd.find_element_by_name("email").send_keys(contact_model.email)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(group1.email2)
+        wd.find_element_by_name("email2").send_keys(contact_model.email2)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(group1.email3)
+        wd.find_element_by_name("email3").send_keys(contact_model.email3)
         # birthday section
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(group1.birth_day)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact_model.birth_day)
         wd.find_element_by_xpath("//option[@value='3']").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(group1.birth_month)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact_model.birth_month)
         wd.find_element_by_xpath("//option[@value='March']").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(group1.birth_year)
+        wd.find_element_by_name("byear").send_keys(contact_model.birth_year)
         # anniversary section
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(group1.ann_day)
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact_model.ann_day)
         wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[18]").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(group1.ann_month)
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact_model.ann_month)
         wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[9]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(group1.ann_year)
+        wd.find_element_by_name("ayear").send_keys(contact_model.ann_year)
         # secondary section
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(group1.address2)
+        wd.find_element_by_name("address2").send_keys(contact_model.address2)
         # submit form
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
