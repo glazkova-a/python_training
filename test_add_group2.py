039eec597621dfb3ce45ca0355a2df61774081cb
anna.g <angie.bts@gmail.com>
2021-11-12 10:13:40 -0500

    The tests passed

--- test_add_group2.py
+++ test_add_group2.py
@@ -5,16 +5,20 @@ import pytest
 from group import Group
 from application import Application
 
+
 @pytest.fixture
 def app(request):
+
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-    
+
+
 def test_add_group2(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="uiuiuiui", header="hjhjhjh", footer="kokokok"))
     app.logout()
+
 
 def test_add_empty_group2(app):
     app.login(username="admin", password="secret")
