347207024d087db72154b27261b06647ef4b5686
anna.g <angie.bts@gmail.com>
2021-11-13 19:30:15 -0500

    test_add_contact & test_add_group got their application (fixture) and helpers.// changed tests' names to the appropriate ones.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+
 import pytest
 from model.contact_model import Contact
 from fixture.application import Application
@@ -15,14 +14,14 @@ def app(request):
 def test_add_new_contact(app):
 
     app.open_home_page()
-    app.login(username="admin", password="secret")
-    app.create_new_contact(Contact(name="A", middlen="B", surname="C", nick="D", company="Company1",
+    app.session.login(username="admin", password="secret")
+    app.contact.create_new_contact(Contact(name="A", middlen="B", surname="C", nick="D", company="Company1",
                                 address="111, Street", home_phone="12345678", mobile_phone="12345678",
                                 work_phone="12345678", email="aaaa@gmail.com", email2="aaaa@gmail.com",
                                 email3="aaaa@gmail.com", birth_day="3", birth_month="March", birth_year="1987",
                                 ann_day="16", ann_month="August", ann_year="2000", address2="222, Street"))
     app.return_to_home_page()
-    app.logout()
+    app.session.logout()
 
 
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,13 +14,13 @@ def app(request):
     return fixture
 
 
-def test_add_group2(app):
+def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="uiuiuiui", header="hjhjhjh", footer="kokokok"))
     app.session.logout()
 
 
-def test_add_empty_group2(app):
+def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
