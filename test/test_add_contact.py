3bd2c4f774a85c8f392c278a4645abeada2d33ab
anna.g <angie.bts@gmail.com>
2021-11-13 20:48:34 -0500

    conftest created, conftest moved to the root,  wd.find_element_by_name("user") added to 'session', 3 out of 3 test passed.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import pytest
+
 from model.contact_model import Contact
-from fixture.application import Application
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_new_contact(app):
 
