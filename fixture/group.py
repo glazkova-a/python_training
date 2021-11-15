868a9509534b1533a0ac28c059f1bf628007d5dc
anna.g <angie.bts@gmail.com>
2021-11-15 12:15:57 -0500

    test_edit_group created and passed

--- fixture/group.py
+++ fixture/group.py
@@ -35,6 +35,24 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
+    def edit_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # click edit button
+        wd.find_element_by_name("edit").click()
+        # clear footer
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        # submit editing
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+
+
+
+
 
     def open_groups_page(self):
         wd = self.app.wd
