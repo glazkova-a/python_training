151e0455c802e29f1d755da439b6dec1b9466cb2
anna.g <angie.bts@gmail.com>
2021-11-19 07:47:10 -0500

    Contact_model was conveyed to test_edit_contact/  wd.find_element_by_link_text("home").click() was conveyed to def del_first_contact and def edit_first_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -73,20 +73,83 @@ class ContactHelper:
         # submit form
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def edit_first_contact(self):
+    def edit_first_contact(self, contact_model):
         wd = self.app.wd
+        # open contact list page
+        wd.find_element_by_link_text("home").click()
         # select first contact
         wd.find_element_by_name("selected[]").click()
         # click "edit" image
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        # clear first name (making some changes with the contact)
+        # edit first contact fields (making some changes with the contact)
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact_model.name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact_model.middlen)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact_model.surname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact_model.nick)
+        # company section
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact_model.company)
+        # address section
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact_model.address)
+        # phones section
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact_model.home_phone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact_model.mobile_phone)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact_model.work_phone)
+        # emails section
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact_model.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact_model.email2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact_model.email3)
+        # birthday section
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact_model.birth_day)
+        wd.find_element_by_xpath("//option[@value='3']").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact_model.birth_month)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact_model.birth_year)
+        # anniversary section
+        wd.find_element_by_name("aday").click()
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact_model.ann_day)
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact_model.ann_month)
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact_model.ann_year)
+        # secondary section
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact_model.address2)
         # submit editing
         wd.find_element_by_name("update").click()
 
     def del_first_contact(self):
         wd = self.app.wd
+        # open contact list page
+        wd.find_element_by_link_text("home").click()
         # select first contact
         wd.find_element_by_name("selected[]").click()
         # select 'delete'
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,7 +1,14 @@
+from model.contact_model import Contact
+
+
 def test_edit_first_contact(app):
 
     app.open_home_page()
     app.session.login(username="admin", password="secret")
-    app.contact.edit_first_contact()
+    app.contact.edit_first_contact(Contact(name="", middlen="", surname="", nick="H", company="Company1",
+                                address="111, Street", home_phone="12345678", mobile_phone="12345678",
+                                work_phone="12345678", email="aaaa@gmail.com", email2="aaaa@gmail.com",
+                                email3="aaaa@gmail.com", birth_day="3", birth_month="March", birth_year="1987",
+                                ann_day="16", ann_month="August", ann_year="1976", address2="222, Street"))
     app.return_to_home_page()
     app.session.logout()
