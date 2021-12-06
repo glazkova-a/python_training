e0c1a92a6c4331dc39e0cb8dcee2bd4563f9613e
anna.g <angie.bts@gmail.com>
2021-12-05 20:26:49 -0500

    test_phones_on_home_page assertion with merge implemented.

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.select import Select
 from model.contact_model import Contact
+import re
 
 
 class ContactHelper:
@@ -41,6 +42,7 @@ class ContactHelper:
         self.name("home", contact_model.home_phone)
         self.name("mobile", contact_model.mobile_phone)
         self.name("work", contact_model.work_phone)
+        self.name("phone2", contact_model.secondary_phone)
         self.name("email", contact_model.email)
         self.name("email2", contact_model.email2)
         self.name("email3", contact_model.email3)
@@ -97,22 +99,50 @@ class ContactHelper:
             self.contact_cache = []
             for element in wd.find_elements_by_name("entry"):
                 cells = element.find_elements_by_tag_name("td")
-                if cells[1]:
-                    surname = cells[1].text
-                if cells[2]:
-                    name = cells[2].text
-                id = element.find_element_by_name("selected[]").get_attribute("id")
-                self.contact_cache.append(Contact(name=name, surname=surname, id=id))
+                surname = cells[1].text
+                name = cells[2].text
+                id = cells[0].find_element_by_tag_name("input").get_attribute("id")
+                all_phones = cells[5].text
+                self.contact_cache.append(
+                    Contact(name=name, surname=surname, id=id, all_phones_from_home_page=all_phones))
+
         return list(self.contact_cache)
 
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
 
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
 
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home_phone = wd.find_element_by_name("home").get_attribute("value")
+        work_phone = wd.find_element_by_name("work").get_attribute("value")
+        mobile_phone = wd.find_element_by_name("mobile").get_attribute("value")
+        secondary_phone = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(name=firstname, surname=lastname, id=id,
+                       home_phone=home_phone, mobile_phone=mobile_phone, work_phone=work_phone,
+                       secondary_phone=secondary_phone)
 
-
-
-
-
-
-
-
-
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home_phone = re.search("H: (.*)", text).group(1)
+        work_phone = re.search("W: (.*)", text).group(1)
+        mobile_phone = re.search("M: (.*)", text).group(1)
+        ## secondary_phone is none --> failed due to splitted str
+        #secondary_phone = re.search("P: (.*)", text).group(1)
+        return Contact(home_phone=home_phone, mobile_phone=mobile_phone, work_phone=work_phone)
--- model/contact_model.py
+++ model/contact_model.py
@@ -3,8 +3,8 @@ from sys import maxsize
 
 class Contact:
     def __init__(self, name=None, middlen=None, surname=None, nick=None, company=None, address=None, home_phone=None,
-                 mobile_phone=None, work_phone=None, email=None, email2=None, email3=None, birth_day=None, birth_month=None,
-                 birth_year=None, ann_day=None, ann_month=None, ann_year=None, address2=None, id=None):
+                 mobile_phone=None, work_phone=None, secondary_phone=None, email=None, email2=None, email3=None, birth_day=None, birth_month=None,
+                 birth_year=None, ann_day=None, ann_month=None, ann_year=None, address2=None, id=None, all_phones_from_home_page=None):
         self.name = name
         self.middlen = middlen
         self.surname = surname
@@ -14,6 +14,7 @@ class Contact:
         self.home_phone = home_phone
         self.mobile_phone = mobile_phone
         self.work_phone = work_phone
+        self.secondary_phone = secondary_phone
         self.email = email
         self.email2 = email2
         self.email3 = email3
@@ -25,12 +26,14 @@ class Contact:
         self.ann_month = ann_month
         self.address2 = address2
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.name)
+        return "%s:%s:%s" % (self.id, self.name, self.surname)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id)\
+               and self.name == other.name and self.surname == other.surname
 
     def id_or_max(self):
         if self.id:
