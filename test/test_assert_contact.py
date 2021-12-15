f4e25728a2e1ee42363cc6c3b89d1f28e79fb31a
anna.g <angie.bts@gmail.com>
2021-12-15 08:40:30 -0500

    test_assert_contact edited

--- test/test_assert_contact.py
+++ test/test_assert_contact.py
@@ -14,10 +14,6 @@ def test_assert_random_contact(app):
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
 
-    # method 1 - assertion works as is without additional functions
-    #assert contact_from_home_page == contact_from_edit_page
-
-    # method 2 - assertion with merge
     assert contact_from_home_page.id == contact_from_edit_page.id
     assert contact_from_home_page.surname == contact_from_edit_page.surname
     assert contact_from_home_page.name == contact_from_edit_page.name
@@ -36,26 +32,9 @@ def merge_phones_like_on_home_page(contact):
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear_emails(x),
-                                filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3]))))
+                            filter(lambda x: x is not None,
+                                   [contact.email, contact.email2, contact.email3])))
 
 
 def clear_phones(s):
     return re.sub('[() -]', "", s)
-
-
-def clear_emails(m):
-    return re.sub('[()]', "", m)
-
-
-
-
-
-
-
-
-
-
-
-
