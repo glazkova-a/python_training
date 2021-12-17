92df4b832931ebb0cbd33a3d163a7abfed36c478
anna.g <angie.bts@gmail.com>
2021-12-17 15:32:56 -0500

    test_del_some_group check_ui (when True - Failed)

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -16,7 +16,7 @@ import random
     #assert old_groups == new_groups
 
 
-def test_del_some_group(app, db):
+def test_del_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
@@ -26,3 +26,5 @@ def test_del_some_group(app, db):
     assert len(old_groups) - 1 == len(new_groups)
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
