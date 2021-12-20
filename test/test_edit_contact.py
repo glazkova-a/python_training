d46434e5b60741ce9b4566feae223eec96772b80
anna.g <angie.bts@gmail.com>
2021-12-20 10:28:17 -0500

    test_edit_contact modified

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,17 +1,18 @@
 from model.contact_model import Contact
 from random import randrange
 
-def test_edit_some_contact(app):
+def test_edit_some_contact(app, db, check_ui):
 
     if app.contact.count_contact_entries() == 0:
         app.contact.create_new_contact(Contact(name="Edit_me", surname=""))
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
     contact = Contact(name="John", surname="Doe")
     contact.id = old_contacts[index].id
     app.contact.edit_contact_by_index(index, contact)
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
     old_contacts[index] = contact
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
