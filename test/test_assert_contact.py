4872e470a7176cbdd41235926b34d5604949a41e
anna.g <angie.bts@gmail.com>
2021-12-20 17:37:21 -0500

    test_assert_all_contacts created

--- test/test_assert_contact.py
+++ test/test_assert_contact.py
@@ -3,6 +3,8 @@ from random import randrange
 import re
 
 
+
+
 def test_assert_random_contact(app):
     if app.contact.count_contact_entries() == 0:
         app.contact.create_new_contact(Contact(name="Assert", surname="Me", address="169, Green St", email="assert@gmail.com",
@@ -38,3 +40,23 @@ def merge_emails_like_on_home_page(contact):
 
 def clear_phones(s):
     return re.sub('[() -]', "", s)
+
+
+
+
+
+def test_assert_all_contacts(app, db):
+    if app.contact.count_contact_entries() == 0:
+        app.contact.create_new_contact(Contact(name="Assert", surname="Me", address="169, Green St", email="assert@gmail.com",
+                                               email2="assert_me@gmail.com", email3="assert_itself@gmail.com", home_phone="0987654",
+                                               mobile_phone="56565656", work_phone="345676543", secondary_phone="+1234567890"))
+        contact_from_home_page = app.contact.get_contact_list()
+        contact_from_db = db.get_contact_list()
+        assert contact_from_home_page.id == contact_from_db.id
+        assert contact_from_home_page.surname == contact_from_db.surname
+        assert contact_from_home_page.name == contact_from_db.name
+        assert contact_from_home_page.address == contact_from_db.address
+        assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
+        assert contact_from_home_page.all_emails == merge_emails_like_on_home_page(contact_from_db)
+
+
