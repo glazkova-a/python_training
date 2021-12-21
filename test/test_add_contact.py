a29798d322a3ca213f38d80c9294bc8b173f00e4
anna.g <angie.bts@gmail.com>
2021-12-21 13:22:38 -0500

    test_del_contact modified

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -18,7 +18,7 @@ def test_add_new_contact(app, json_contacts, db, check_ui):
     new_contacts = db.get_contact_list()
     old_contacts.append(contact)
     if check_ui:
-        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list, key=Contact.id_or_max)
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(old_contacts, key=Contact.id_or_max)
 
 
 
