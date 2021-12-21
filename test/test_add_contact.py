d360dc581900db8037c936ef732498dec3cc1278
anna.g <angie.bts@gmail.com>
2021-12-21 08:26:42 -0500

    test_add_group_to_contact modified

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from model.contact_model import Contact
+from fixture.orm import ORMFixture
 import pytest
 import random
 import string
@@ -20,11 +21,13 @@ def test_add_new_contact(app, json_contacts, db, check_ui):
         assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_add_group_to_contact(app, data_contacts, orm, check_ui):
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+def test_add_group_to_contact(app, data_contacts, db, check_ui):
     contact = data_contacts
-    old_contacts = orm.get_contacts_in_group()
+    old_contacts = db.get_contacts_in_group()
     app.contact.create_new_contact(contact)
-    new_contacts = orm.get_contacts_in_group()
+    new_contacts = db.get_contacts_in_group()
     old_contacts.append(contact)
     assert len(old_contacts)-1 == new_contacts
     if check_ui:
